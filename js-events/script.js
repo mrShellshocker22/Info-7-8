a2b3649e4223fefe5d7c6e52cb37217eaf8f9342
mrShellshocker22 <mrShellshocker22@gmail.com>
2026-04-20 17:28:43 +0000

    dgdg

---
+++ js-events/script.js
@@ -0,0 +1,11 @@
+function article1(){
+    alert('We believe in God, the Eternal Father, and in His Son, Jesus Christ, and in the Holy Ghost.');
+}
+
+function article2(){
+    alert('We believe that men will be punished for their own sins, and not for Adam’s transgression.');
+}
+
+function article3(){
+    alert('We believe that through the Atonement of Christ, all mankind may be saved, by obedience to the laws and ordinances of the Gospel.');
+}
