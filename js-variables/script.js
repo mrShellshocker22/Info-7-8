1e55d2787b2c7824f36e77429f3908c712cf28cf
mrShellshocker22 <mrShellshocker22@gmail.com>
2026-04-22 20:18:57 +0000

    ruru

--- js-variables/script.js
+++ js-variables/script.js
@@ -1,1 +1,28 @@
-function1()
+function function1(){
+const Name = "Gabriel";
+const favoriteColor = "Blue";
+let Location = "Colonia Juarez";
+let Mood = "67";
+
+console.log(Name + " " + favoriteColor + " " + Location + " " + Mood )
+}
+
+function function2(){
+let companyName = "Dell inc";
+let foundingYear = "1984";
+let isActive = "yes";
+let fundingAmount = "33.4 Billion Bucks";
+
+console.log(companyName + " " + foundingYear + " " + isActive + " " + fundingAmount )
+}
+
+function function3(){
+    
+let doubleIt =
+    - Double it.
+- Add 10.
+- Divide by two.
+- Subtract the number you started with.
+
+}
+
