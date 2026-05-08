ac6879576d95b9915cf1e0ec7fe0c7fcf9206a7c
mrShellshocker22 <mrShellshocker22@gmail.com>
2026-05-08 20:06:35 +0000

    btbtb

--- greeting/script.js
+++ greeting/script.js
@@ -17,15 +17,32 @@ document.addEventListener("DOMContentLoaded", function(){
             document.querySelector("#time").innerHTML = "🌙 Good night!";
         }
 
-        let day = get.Day();
+        let day = now.getDay();
 
         if (day == 0){
             document.querySelector("#day").innerHTML = "Today is Sunday!";
+            document.querySelector("#friday").innerHTML = "Enjoy your Weekend";
         }   else if (day == 1){
             document.querySelector("#day").innerHTML = "Today is Monday!";
+            document.querySelector("#friday").innerHTML = "Not Friday yet 😒";
         }   else if (day == 2){
-            document.querySelector("#day").innerHTML = "Today is Monday!";
+            document.querySelector("#day").innerHTML = "Today is Tuesday!";
+            document.querySelector("#friday").innerHTML = "Not Friday yet 😒";
+        }   else if (day == 3){
+            document.querySelector("#day").innerHTML = "Today is Wednesday!";
+            document.querySelector("#friday").innerHTML = "Not Friday yet 😒";
+        }   else if (day == 4){
+            document.querySelector("#day").innerHTML = "Today is Thursday!";
+            document.querySelector("#friday").innerHTML = "Not Friday yet 😒";
+        }   else if (day == 5){
+            document.querySelector("#day").innerHTML = "Today is Friday!";
+            document.querySelector("#friday").innerHTML = "Finally Friday!";
+        }   else if (day == 6){
+            document.querySelector("#day").innerHTML = "Today is Sunday!";
+            document.querySelector("#friday").innerHTML = "Enjoy your Weekend!";
         }
+
+
 
 
         return false;
