a24b425b372465940221d97ce82b95aa9180f900
mrShellshocker22 <mrShellshocker22@gmail.com>
2026-05-12 16:39:21 +0000

    dgdg

--- interval/script.js
+++ interval/script.js
@@ -1,18 +1,31 @@
-        let counter = 5;
+        let counter = 12;
+        const alarm = new Audio("alarm.mp3")
+
+        if ()
+
 
 function count() {
 
-    document.querySelector('h1').innerHTML = counter;
-    
+    let minutes = Math.floor(counter/60);
+    let seconds = counter % 60;
+
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }
+
+    document.querySelector('h1').innerHTML = minutes + ":" + seconds;
+
+
     if (counter > 0){
         counter--;
-
-        const alarm = new Audio("alarm.mp3")
-            kick.currentTime = 0;
-            kick.play();
-
     }
+    else if (counter == 0){
+            alarm.currentTime = 0;
+            alarm.play();
+    }
+
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button').onclick = function(){
